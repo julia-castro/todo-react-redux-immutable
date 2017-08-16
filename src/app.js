b8c7f1ce5fe99925538d1780315fd561864796dc
Julia Castro <castro.morales.julia@gmail.com>
2017-08-16 17:55:30 -0400

    create store

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
 import { TodoList } from './components';
 import { List, Map } from 'immutable';
+import reducer from './reducer';
 
 const dummyTodos = [
   Map({id: 1, isDone: true, text: 'make components'}),
@@ -9,6 +11,8 @@ const dummyTodos = [
   Map({id: 3, isDone: false, text: 'implement reducer'}),
   Map({id: 4, isDone: false, text: 'connect components'}),
 ];
+
+const store = createStore(reducer);
 
 render(
   <TodoList todos={dummyTodos} />,
