f1d1cb292c1758bb854aa9f340df805aaa170912
Julia Castro <castro.morales.julia@gmail.com>
2017-08-17 12:51:50 -0400

    add comments to help understand react-redux

--- src/containers.js
+++ src/containers.js
@@ -5,11 +5,16 @@ import { addTodo, toggleTodo } from './actions';
 export const TodoList = connect(
   function mapStateToProps(state) {
     return { todos: state}
+    // it might help to visualize this on an instance of the wrapped React component
+    // <TodoList todos={state}
   },
   function mapDispatchToProps(dispatch) {
+    //this function is passed the store's dispatch method, so that we can use it to dispatch the actions from our action creators
     return {
       addTodo: text => dispatch(addTodo(text)),
       toggleTodo: id => dispatch(toggleTodo(id))
+      // might help to visualize all these props together on an instance of our wrapped React component
+      // <TodoList todos={state} addTodo={text => dispatch(addTodo(text))} toggleTodo={id => dispatch(toggleTodo(id))} />
     }
   }
 )(components.TodoList)
